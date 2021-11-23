2c88ed738bcd2ebb7c4cc95f2c8419bbfc18c2be
naa15 <naltu15@freeuni.edu.ge>
2021-11-23 20:21:14 +0400

    style: make little style changes on list quotes page

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,35 +1,39 @@
 @extends('layout')
 
-
-<div class="flex h-screen">
-    {{-- language buttons --}}
+@section('content')
     
-    <div class="space-y-2 m-auto flex-shrink-0 mr-16 ml-16">
-        <a href="/en/movie/{{ $movie->slug }}" class="relative flex">
-            <img class="" src="/images/circle2.png" alt="">
-            <h4 class="text-2xl absolute inset-1/4 text-white">en</h4>
-        </a>
-
-        <a href="/ka/movie/{{ $movie->slug }}" class="relative flex">
-            <img class="" src="/images/circle.png" alt="">
-            <h4 class="text-2xl absolute inset-1/4">ka<h4>
-        </a>
-    </div>
-
-    <div class="text-center flex-1 mt-56">
-        <h1 class="text-5xl mt-24 text-white underline mb-14">
-            {{ ucwords($movie->title) }}
-        </h1>
-
-        @foreach ($quotes as $quote)
-            <div class="mb-14 inline-grid">
-                <img src="{{ $quote->thumbnail ? asset('storage/' . $quote->thumbnail) : '/images/image.png' }}"
-                    class="m-auto rounded-xl" alt="" width="700">
-                <h1 class="text-5xl mt-16 text-white max-w-5xl">
-                    "{{ $quote->body }}"
-                </h1>
+    <div class="flex h-screen">
+        {{-- language buttons --}}
+    
+        <div class="space-y-2 m-auto flex-shrink-0 mr-16 ml-16">
+            <a href="/en/movie/{{ $movie->slug }}" class="relative flex">
+                <img class="" src="/images/circle2.png" alt="">
+                <h4 class="text-2xl absolute inset-1/4 text-white">en</h4>
+            </a>
+    
+            <a href="/ka/movie/{{ $movie->slug }}" class="relative flex">
+                <img class="" src="/images/circle.png" alt="">
+                <h4 class="text-2xl absolute inset-1/4">ka<h4>
+            </a>
+        </div>
+    
+        <div class="text-center flex-1">
+            <h1 class="text-5xl mt-24 text-white mb-14">
+                {{ ucwords($movie->title) }}
+            </h1>
+    
+            <div class="flex flex-col">
+                @foreach ($quotes as $quote)
+                    <div class="flex flex-col items-center mb-14">
+                        <img src="{{ $quote->thumbnail ? asset('storage/' . $quote->thumbnail) : '/images/image.png' }}"
+                            class="m-auto rounded-b-none" alt="" width="768">
+                        <h1 class="bg-white leading-normal px-20 py-10 text-5xl text-black rounded-b-xl max-w-screen-md" style="width:768px">
+                            "{{ $quote->body }}"
+                        </h1>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
-
+    
+        </div>
     </div>
-</div>
+@endsection
