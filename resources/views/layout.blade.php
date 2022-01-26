b43dd8c09379ef07cb5fe201e1db84fc2025bc25
naa15 <naltu15@freeuni.edu.ge>
2022-01-26 14:12:03 +0400

    style: etract css into resources/css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,13 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
-
-    <style>
-        body {
-            background: radial-gradient(circle at 50% 50%, #4E4E4E 0%, #3D3B3B 99.97%, #484848 99.98%, #3D3B3B 99.99%, #3D3B3B 100%);
-        }
-
-    </style>
 </head>
 
 
