1e77a2d8ddc94d004d89df9703ee15a6fde0b034
naa15 <naltu15@freeuni.edu.ge>
2022-01-26 14:16:04 +0400

    refactor: npm install alpine instead of using cdn

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
 </head>
 
 
